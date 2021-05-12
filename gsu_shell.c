f9f96984865b52ab986d79fb152cbece87cb7b89
berkayyildiz <berkayyildizgm@gmail.com>
2021-05-12 10:34:12 +0300

    new line fix and better indentation

--- gsu_shell.c
+++ gsu_shell.c
@@ -10,32 +10,46 @@
 #include "parser.h"
 
 #define VERSION "1.0"
-#define PROMPT  ">"
+#define PROMPT ">"
 
-int main(int argc, char *argv[]) {
-    /* The shell functions as an infinite loop
+/* Number of processes has been done to determine should add new line when print prompt */
+int process_count = 0;
+
+int main(int argc, char *argv[])
+{
+  /* The shell functions as an infinite loop
      * until interrupted. */
-    while (1) {
-        int argc = 0;
-        pid_t cpid;
-        /* TODO: Print the prompty string using PROMPT constant */
-        printf("%s",PROMPT);  
-        
-        /* Read a line from terminal and tokenize it from spaces into
-         * an array of strings. */
-        char **argv = shell_process_line(&argc);
+  while (1)
+  {
+    int argc = 0;
+    pid_t cpid;
+    /* TODO: Print the prompty string using PROMPT constant */
+    if (process_count)
+    {
+      printf("\n%s", PROMPT);
+    }
+    else
+    {
+      printf("%s", PROMPT);
+    }
 
-        /* If argv == NULL, the user just pressed Enter key without any
+    /* Read a line from terminal and tokenize it from spaces into
+         * an array of strings. */
+    char **argv = shell_process_line(&argc);
+    process_count++;
+    /* If argv == NULL, the user just pressed Enter key without any
          * command. */
 
-        if (argv) {
-            /* TODO: Add support for exit and version commands.
+    if (argv)
+    {
+      /* TODO: Add support for exit and version commands.
              *
              * if argv[0] is "version" or "exit", you have to process
              * them right here. */
 
-            if ( strcmp(argv[0],"version") == 0) {
-                /* TODO: If command == version, print the version
+      if (strcmp(argv[0], "version") == 0)
+      {
+        /* TODO: If command == version, print the version
                  * of the shell. Example usage scenario from terminal:
                  *
                  * > version
@@ -47,43 +61,46 @@ int main(int argc, char *argv[]) {
                  * Also, free the argv array using
                  * the relevant function from parser.c
                  * */
-                 printf("GSU Shell Version: %s" ,VERSION); 
-            }
+        printf("GSU Shell Version: %s", VERSION);
+      }
 
-            else if ( strcmp(argv[0],"exit") == 0) {
-                /* TODO */
-                /* If command == exit, you should terminate
+      else if (strcmp(argv[0], "exit") == 0)
+      {
+        /* TODO */
+        /* If command == exit, you should terminate
                  * the shell using exit(). The return value should denote
                  * a success (0 or EXIT_SUCCESS).
                  *
                  * You also have to free the argv array using
                  * the relevant function from parser.c before exiting.
                  */
-                 return EXIT_SUCCESS;
-                 printf("GSU Shell'i çalıştıran ana kabuga geri döndük");                 
-            }
+        return EXIT_SUCCESS;
+        printf("GSU Shell'i çalıştıran ana kabuga geri döndük");
+      }
 
-            /* If the typed command is not a built-in one (like exit or
+      /* If the typed command is not a built-in one (like exit or
              * version), you have to fork a new process and the new child
              * process should execute the given command using execvp().
              *
              */
-            else {
-                /* This is to store the PID of the child process */
-                pid_t child_pid = -1;
-               
-                /* TODO: Now it's time to fork! Go back to TP from moodle if you
+      else
+      {
+        /* This is to store the PID of the child process */
+        pid_t child_pid = -1;
+
+        /* TODO: Now it's time to fork! Go back to TP from moodle if you
                  * do not understand the below switch structure.
                  */
-                switch (child_pid = fork()) {
-                    case -1:
-                        perror("fork");
-                        break;
-                    case 0: ;
-                        /*****************/
-                        /* Child process */
-                        /*****************/
-                        /* TODO: Child process should now execute the program.
+        switch (child_pid = fork())
+        {
+        case -1:
+          perror("fork");
+          break;
+        case 0:;
+          /*****************/
+          /* Child process */
+          /*****************/
+          /* TODO: Child process should now execute the program.
                          * You should also give all the arguments with the
                          * command.
                          * Example:
@@ -92,12 +109,12 @@ int main(int argc, char *argv[]) {
                          * you have argv  = {"ls", "-l", "--color", NULL}
                          * When you execvp(), make sure you pass the
                          * parameters correctly! */
-                         char a[1000];
-                         strcpy(a,argv[0]);
-                         char *args[]= {a, a, NULL};                      
-                         execvp("/usr/sbin", args);
+          char a[1000];
+          strcpy(a, argv[0]);
+          char *args[] = {a, a, NULL};
+          execvp("/usr/sbin", args);
 
-                        /* TODO: If the code reaches here, it means that execvp()
+          /* TODO: If the code reaches here, it means that execvp()
                          * failed for some reason. Investigate errno
                          * variable and print the problem.
                          *
@@ -118,54 +135,58 @@ int main(int argc, char *argv[]) {
                          * with the return code 126 whereas in the second case
                          * 127.
                          */
-                         if(errno == EACCES){
-                         printf("Persmission denied.");
-                         exit(126);
-                         }
-                         else if(errno == ENOENT){
-                         printf("The file does not exist.");  
-                         exit(127);                       
-                         }
-                         
-                        /* TODO: Free argv array. */
-                         shell_free_args(argv); 
-                         
-                        /* TODO: Exit with the relevant status code. */
-                         exit(0);
-                        break;
-                        
-                    default:
-                        /******************/
-                        /* Parent process */
-                        /******************/
+          if (errno == EACCES)
+          {
+            printf("Persmission denied.");
+            exit(126);
+          }
+          else if (errno == ENOENT)
+          {
+            printf("The file does not exist.");
+            exit(127);
+          }
 
-                        /* TODO: No need for the argv array in the parent process. Free it. */
-                         shell_free_args(argv);
-                         
-                        /* Define variables status and child_retval */
-                        int status;
-                        int child_retval = -1;
+          /* TODO: Free argv array. */
+          shell_free_args(argv);
 
-                        /* TODO: Wait for the forked child to complete. */
+          /* TODO: Exit with the relevant status code. */
+          exit(0);
+          break;
 
-                         cpid = waitpid(child_pid,&status,0);
-                         child_retval = status;
-                        /* TODO: If the child process exited normally
+        default:
+          /******************/
+          /* Parent process */
+          /******************/
+
+          /* TODO: No need for the argv array in the parent process. Free it. */
+          shell_free_args(argv);
+
+          /* Define variables status and child_retval */
+          int status;
+          int child_retval = -1;
+
+          /* TODO: Wait for the forked child to complete. */
+
+          cpid = waitpid(child_pid, &status, 0);
+          child_retval = status;
+          /* TODO: If the child process exited normally
                          * store the return value of the child process into
                          * child_retval variable (See TP1) */
-                         if (WIFSIGNALED(status)){
-                         printf("Error:%d ",WTERMSIG(status));
-                         }
-                         else if (WIFEXITED(status)){
-                         printf("Exited Normally and x:%d\n ",WEXITSTATUS(status));
-                         }
-                         exit(EXIT_SUCCESS);
+          if (WIFSIGNALED(status))
+          {
+            printf("Error:%d ", WTERMSIG(status));
+          }
+          else if (WIFEXITED(status))
+          {
+            printf("Exited Normally and x:%d\n ", WEXITSTATUS(status));
+          }
+          exit(EXIT_SUCCESS);
 
-                        break;
-                } /* switch */
-            } /* else */
-        } /* if (argv) */
-    } /* while(1) */
+          break;
+        } /* switch */
+      }   /* else */
+    }     /* if (argv) */
+  }       /* while(1) */
 
-    return 0;
+  return 0;
 }
